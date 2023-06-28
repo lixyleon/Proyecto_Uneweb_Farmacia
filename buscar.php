9e5970e529889dacf3dcad75a30fe333a2cbabec
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-06-27 23:15:14 -0400

    Culminacion del buscador Cambios en el código para el buscador sustituyendo el while por el foreach se ajusto para mostrar el nombre de la sucursal en sustitucion del id_sucursal

--- buscar.php
+++ buscar.php
@@ -3,52 +3,66 @@
 include("administrador/config/bd.php");
 
 $busqueda = (!empty($_POST['busqueda'])) ? $_POST['busqueda'] : '';
-
-//consultas
-
 $query = "SELECT * FROM inventario WHERE nombre LIKE '%$busqueda%'";
 $sentencia = $conexion->query($query);
-$num_rows = count($sentencia->fetch(PDO::FETCH_ASSOC));
-
+$listaproducto = $sentencia->fetchAll(PDO::FETCH_OBJ);
+$row = count($listaproducto);
+//echo $row;
 ?>
 
-
 <h1 class="text-center text-primary m-2">Consulta tus productos</h1>
-
-
 <form class="d-flex m-4" role="search" method="POST">
     <input class="form-control me-2" type="search" id="search" placeholder="Buscar" name="busqueda" aria-label="Buscar" require>
     <button class="btn btn-outline-primary" type="submit">Buscar</button>
 </form>
 
 <div class="container-fluid">
-    <?php
-
-    if ( isset($_POST['busqueda']) and $busqueda != '') {
-        echo '<table class="table  table-responsive table-striped mb-5">';
-        if ($num_rows > 0) {
-            echo '<tr class="table-primary"><th>Nombre</th><th>Cantidad</th><th>Precio</th><th>Sucursal</th></tr>';
-            while ($data = $sentencia->fetch(PDO::FETCH_ASSOC)) {
-                echo '<tr class="table-primary">';
-                echo "<td>" . $data["nombre"] . "</td>";
-                echo "<td>" . $data["cantidad"] . "</td>";
-                echo "<td>" . $data["precio"] . "</td>";
-                echo "<td>" . $data["id_sucursal"] . "</td></tr>";
-                
-            }
-            echo "</table>";
-        } else {
-            echo "<div>No se encontraron resultados</div>";
-        }
-    }
-
-
-    ?>
-
-
-
-
+<?php if ( isset($_POST['busqueda']) & $row>0 ) {?>
+      <table class="table  table-responsive table-striped">
+       <tr class="table-primary">
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Precio</th>
+            <th>Sucursal</th>
+        </tr>
+        <?php   foreach ($listaproducto as $producto) {?>
+            <tr class="table-primary">
+                <td><?php echo $producto->nombre; ?></td>
+                <td><?php echo $producto->cantidad; ?></td>
+                <td><?php echo $producto->precio; ?></td>
+                <td>
+                    <?php 
+                    $sentencia3 = $conexion->query("SELECT * FROM sucursales WHERE id = $producto->id_sucursal ");
+                    $filas = $sentencia3->fetchAll(PDO::FETCH_OBJ);
+                    foreach ($filas as $fila) {
+                        echo ($fila -> nombre);
+                    }
+  
+                    ?>
+            
+            </td>
+            </tr>
+        <?php }
+        echo "</table>";
+     } else {?>
+            <div class = "container-fluid m-3 text-center fs-4">
+            No se encontraron resultados
+            </div>
+<?php  } ?>
+</div>
 
 
 
 <?php include("template/pie_pagina.php"); ?>
+
+
+
+
+
+
+
+
+
+
+
+
