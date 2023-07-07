3ed0a790dba8c18e94c891917d23d72903508fb8
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-06 22:06:28 -0400

    Desarrollo de las secciones Nosotros y Tiendas

--- tiendas.php
+++ tiendas.php
@@ -1,4 +1,37 @@
 <?php include("template/cabecera.php"); ?>
 
+<div class="card-group m-4">
+  <div class="card h-100">
+    <img src="imagenes/contento-mujer-pelo-largo-mirando-contento-despues-comprar-productos-farmacia.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">La Florida</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Imagen de senivpetro en Freepik</small>
+    </div>
+  </div>
+  <div class="card h-100">
+    <img src="imagenes/farmaceutico-comprobando-medicamentos-farmacia.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Altamira</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Imagen de senivpetro en Freepik</small>
+    </div>
+  </div>
+  <div class="card h-100">
+    <img src="imagenes/farmaceutico-joven-farmacia.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Los Chaguaramos</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Imagen de senivpetro en Freepik</small>
+    </div>
+  </div>
+</div>
+
 
 <?php include("template/pie_pagina.php"); ?>
