b1a54de523aa1b42993b843715dddc9b67e67fd2
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-08 20:14:13 -0400

    coreccion de tajeta sobre servicios de la pagina principal

--- index.php
+++ index.php
@@ -206,8 +206,8 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-eyeglasses img-fluid mb-3 text-primary" viewBox="0 0 16 16">
                 <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
               </svg>
-              <h3 class="text-center">Equipos médicos</h3>
-              <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis aspernatur culpa deleniti sunt nihil dolore tempore, quae fugiat, error sed recusandae rem in, impedit qui cumque nisi quibusdam ipsam aliquam.</p>
+              <h3 class="text-center">Cuidamos tu salud visual</h3>
+              <p class="">Lorem ipsum dolor sit amet consectetur. Officiis aspernatur culpa deleniti sunt nihil dolore tempore, quae fugiat, error sed recusandae rem in, impedit qui cumque nisi quibusdam ipsam aliquam.</p>
               <button type="button" class="btn btn-primary">
               <a class="text-white" href="productos.php">Conoce más</a></button>
             </div>
