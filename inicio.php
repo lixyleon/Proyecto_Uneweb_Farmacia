8b643bb7cd9a8f9b5300628e421ed005c5988688
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-07-28 22:43:13 -0400

    correccion de login
    correccion del login en la clave

---
+++ inicio.php
@@ -0,0 +1,16 @@
+<?php
+include("template/cabecera.php");
+
+
+
+?>
+
+<div class="container m-4">
+<h1 class="display-5 fw-bold text-danger">Bienvenid@</h1>
+      <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. A quos ducimus vitae exercitationem quaerat quas aliquid obcaecati beatae sunt? Reiciendis ut quibusdam at placeat odio officia aperiam incidunt itaque nulla!.</p>
+</div>
+
+  
+  <?php
+  include("template/pie.php");
+  ?>
